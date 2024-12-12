eb3be715fddc2b7e8e56ef4c765731ad36850a7a
huynvtasc <huynv@tasc.vn>
2024-12-12 18:13:44 +0700

    init commit

---
+++ NguyenVanHuyTest/src/main/java/com/example/NguyenVanHuyTest/NguyenVanHuyTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.NguyenVanHuyTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NguyenVanHuyTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NguyenVanHuyTestApplication.class, args);
+	}
+
+}
